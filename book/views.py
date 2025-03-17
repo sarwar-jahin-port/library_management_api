3fe76f2c2141fe89e9e1fe2fb1085073966c5040
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-03-17 12:17:47 -0700

    practice problem 26.5 completed

--- book/views.py
+++ book/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from book.models import Book, Author, Category
 from book.serializers import BooksSerializer, AuthorSerializer, CategorySerializer
 from book.paginations import DefaultPagination
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 
 # Create your views here.
 
@@ -10,11 +11,14 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     pagination_class = DefaultPagination
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
--- user/views.py
+++ user/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render
-<<<<<<< Updated upstream
-
-# Create your views here.
-=======
 from rest_framework.viewsets import ModelViewSet
 from user.models import Member
 from user.serializers import UserCreateSerialier
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 
 # Create your views here.
 
 class MemberViewSet(ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = UserCreateSerialier
->>>>>>> Stashed changes
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
